ee9d02a98fddde52f60497a919dce373f147ac9f
zobiasalman <45417354+zobiasalman@users.noreply.github.com>
2018-11-28 16:20:55 +0500

    Update sserver.php

--- sserver.php
+++ sserver.php
@@ -6,7 +6,7 @@ $NAME= "";
 $CONTACT = "";
 $LIST = "";
 $edit_state = false;
-$db= mysqli_connect('localhost', 'root', 'marium95', 'mysql');
+$db= mysqli_connect('localhost', 'root', 'Abcd#1234', 'mysql');
 
 if(isset ($_POST['save'])){
 $ID = $_POST['ID'];
