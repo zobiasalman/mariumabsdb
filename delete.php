e5b5154b095218105191bc8ac3790e2d026a8420
zobiasalman <45417354+zobiasalman@users.noreply.github.com>
2018-11-28 20:36:37 +0500

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,5 +1,5 @@
 <?php 
- $conn = mysqli_connect("localhost", "root", "", "mysql"); 
+ $conn = mysqli_connect("localhost", "root", "Abcd#1234", "mysql"); 
 $id=isset($_POST["id"])?$_POST["id"]:"";
  $sql = "DELETE FROM SALESORDER13165 WHERE ORDERNO = $id"; 
  if(mysqli_query($conn, $sql)) 
