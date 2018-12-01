d5de3861d0841a142a7bb5e7234bea60a91b6126
zobiasalman <45417354+zobiasalman@users.noreply.github.com>
2018-12-01 06:18:07 +0500

    Update create.php

--- create.php
+++ create.php
@@ -1,21 +1,21 @@
 <?php  
- $connect = mysqli_connect("localhost", "root", "Abcd#1234", "mysql"); 
-$ORDERNO=isset($_POST["ORDERNO"])?$_POST["ORDERNO"]:"";
-$CID=isset($_POST["CID"])?$_POST["CID"]:"";
-$DATE=isset($_POST["DATE"])?$_POST["DATE"]:"";
-$ID=isset($_POST["ID"])?$_POST["ID"]:"";
-$CODE=isset($_POST["CODE"])?$_POST["CODE"]:"";
-$QUANTITY=isset($_POST["QUANTITY"])?$_POST["QUANTITY"]:"";
-$RATE=isset($_POST["RATE"])?$_POST["RATE"]:"";
-$AMOUNT=isset($_POST["AMOUNT"])?$_POST["AMOUNT"]:"";
 
+ $ORDERNO='';
+ $CID='';
+ $DATE='';
+ $ID='';
+ $CODE='';
+ $QUANTITY='';
+ $RATE='';
+ $AMOUNT='';
+ $connect = mysqli_connect("localhost", "root", "Abcd#1234", "mysql");  
  $res = mysqli_query($connect, "SELECT PRICE FROM PRODUCTS13165 WHERE CODE='$CODE'");
- $row = mysqli_fetch_array($res);     
-$RATE=$row["PRICE"];
- $sql = "INSERT INTO SALESORDER13165 VALUES('$ORDERNO','$CID','$DATE','$ID','$CODE','$QUANTITY','$RATE','$AMOUNT')";  
+ $row = mysqli_fetch_array($res);
+ $sql= "INSERT INTO SALESORDER13165(ORDERNO,CID,DATE,ID,CODE,QUANTITY,RATE,AMOUNT) VALUES ('$ORDERNO','$CID','$DATE','$ID','$CODE','$QUANTITY','$RATE','$AMOUNT')";
+ /*$sql = "INSERT INTO SALESORDER13165 VALUES('".$_POST["ORDERNO"]."', '".$_POST["CID"]."', '".$_POST["DATE"]."', '".$_POST["ID"]."', '".$_POST["CODE"]."', '".$_POST["QUANTITY"]."', '".$row["RATE"]."', '".$_POST["AMOUNT"]."')"; */ 
  if(mysqli_query($connect, $sql))  
  {  
-      mysqli_query($connect, "UPDATE SALESORDER13165 SET AMOUNT=($RATE*$QUANTITY) WHERE ORDERNO='".$_POST["ORDERNO"]."'");
+      mysqli_query($connect, "UPDATE SALESORDER13165 SET AMOUNT=RATE*QUANTITY WHERE ORDERNO='".$_POST["ORDERNO"]."'");
       echo 'Data Inserted';  
  }  
- ?>
+ ?> 
