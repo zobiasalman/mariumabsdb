0f4fc7b0373bcd564f562b1715575fd1867ddcc4
zobiasalman <45417354+zobiasalman@users.noreply.github.com>
2018-12-01 06:24:33 +0500

    Update salesperson.php

--- salesperson.php
+++ salesperson.php
@@ -12,6 +12,56 @@
 	
 	}
 ?>
+<style>
+
+/* Add a black background color to the top navigation */
+.nav {
+    background-color: #333;
+    overflow: hidden;
+}
+
+/* Style the links inside the navigation bar */
+.nav a {
+    float: left;
+    color: #f2f2f2;
+    text-align: center;
+    padding: 14px 16px;
+    text-decoration: none;
+    font-size: 17px;
+}
+
+/* Change the color of links on hover */
+.nav a:hover {
+    background-color: #ddd;
+    color: black;
+}
+
+/* Add a color to the active/current link */
+.nav a.active {
+    background-color: #4CAF50;
+    color: white;
+}
+
+</style>
+
+
+
+
+     
+<div class="nav">
+  <a href='home.php'><b>HOME</b></a>
+  <a href='database.php'><b>Customer</b></a>
+  <a  class="active" href='salesperson.php'><b>SalesPerson</b></a>
+  <a href='users.php'><b>User</b></a>
+  <a href='products.php'><b>Product</b></a>
+  <a href='table.php'><b>SalesOrder</b></a>
+
+  <a  href='login13165.php'><b>LOGOUT</b></a>
+
+
+</div>
+
+</style>
 
 <!DOCTYPE html>
 <html>
